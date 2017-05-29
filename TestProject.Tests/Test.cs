bf252b3a261fbcca391cf359369bcef4761526c8
Anthony Verslues <anthony@Anthonys-MacBook-Pro.local>
2017-05-29 13:33:29 -0400

    test

---
+++ TestProject.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestProject.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
